2601db39050221d67d3ee7f05a1a4c99c5071511
HyojunK <gywns0608@naver.com>
2023-06-29 21:15:11 +0900

    first commit

---
+++ lib/providers/active_todo_count.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/todo_list.dart';
+
+class ActiveTodoCountState extends Equatable {
+  final int activeTodoCount;
+  ActiveTodoCountState({
+    required this.activeTodoCount,
+  });
+
+  factory ActiveTodoCountState.initial() {
+    return ActiveTodoCountState(activeTodoCount: 0);
+  }
+
+  @override
+  List<Object> get props => [activeTodoCount];
+
+  @override
+  bool get stringify => true;
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+}
+
+class ActiveTodoCount {
+  final TodoList todoList;
+  ActiveTodoCount({
+    required this.todoList,
+  });
+
+  ActiveTodoCountState get state => ActiveTodoCountState(
+      activeTodoCount: todoList.state.todos
+          .where((Todo todo) => !todo.completed)
+          .toList()
+          .length);
+}
